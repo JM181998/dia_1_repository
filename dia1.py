bd311de9df6604434dc51353d89b6b80950684fc
Juanma <juanmaf98@gmail.com>
2025-01-29 21:29:36 +0100

    paso código para que funcione con streamlit

--- dia1.py
+++ dia1.py
@@ -1,10 +1,12 @@
 import pandas as pd
+import streamlit as st
 
 def main():
-    print("hola")
-    print("adios")
-    df=pd.read_csv("https://raw.githubusercontent.com/R43ed/Data_Visualization/refs/heads/main/summer.csv)
-    print(df.head)    
+    st.write("hola")
+    st.write("adios")
+    df=pd.read_csv("https://raw.githubusercontent.com/R43ed/Data_Visualization/refs/heads/main/summer.csv")
+    st.dataframe(df.head())
+     
 if __name__=="__main__":
     main()
  
