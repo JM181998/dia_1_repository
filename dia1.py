7bdd0262335c110dbd3db5c21a21b9b8d74deba8
Juanma <juanmaf98@gmail.com>
2025-01-29 20:47:54 +0100

    estoy intentando meter un df

--- dia1.py
+++ dia1.py
@@ -1,6 +1,10 @@
+import pandas as pd
+
 def main():
     print("hola")
     print("adios")
+    df=pd.read_csv("https://raw.githubusercontent.com/R43ed/Data_Visualization/refs/heads/main/summer.csv)
+    print(df.head)    
 if __name__=="__main__":
     main()
  
